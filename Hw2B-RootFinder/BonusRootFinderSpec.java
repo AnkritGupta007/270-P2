42943310ba196cf7172db82c2c7f583455df22cd
Ankrit Gupta <56211352+AnkritGupta007@users.noreply.github.com>
2021-02-20 20:01:54 -0500

    Update BonusRootFinderSpec.java
    Added three test cases.

--- Hw2B-RootFinder/BonusRootFinderSpec.java
+++ Hw2B-RootFinder/BonusRootFinderSpec.java
@@ -36,5 +36,36 @@ class BonusRootFinderSpec {
 		    rf.runSecantMethod(.25, .5, 1e-5, 1);
 		});
 	}
+	
+	@Test
+	void runBisectionMethodFe_x_1() throws Exception {
+		Func f = (double x) -> (Math.exp(x) - 1);
+		RootFinder rf = new RootFinder(-2.5, 2.5, f);
+		double zero = rf.runBisectionMethod(1e-5, 40);
+		
+		assertTrue(Math.abs(zero - 0) < 1e-5);
+		
+	}
+	
+	@Test
+	void runNewtonsMethodFX_sqroot_1() throws Exception {
+		Func f = (double x) -> (Math.pow(x, 0.5) - 1);
+		Func fprime = (double x) -> (0.5 * Math.pow(x, -0.5));
+		RootFinder rf = new RootFinder(-2.5, 2.5, f);
+		double zero = rf.runNewtonsMethod(.5, fprime, 1e-5, 50);
+		
+		assertTrue(Math.abs(zero - 1) < 1e-5);
+		
+	}
+	
+	@Test
+	void runSecantMethod_e_x_1() throws Exception {
+		Func f = (double x) -> (Math.exp(x) - 1);
+		RootFinder rf = new RootFinder(-2.5, 2.5, f);
+		double zero = rf.runSecantMethod(-2.5, 2.5, 1e-5, 50);
+		
+		assertTrue(Math.abs(zero - 0) < 1e-5);
+		
+	}
 
 }
